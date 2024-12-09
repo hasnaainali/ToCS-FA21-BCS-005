31f6e2fe9cd307c56344b9be88e2d957671f937d
hasnaainali <134831998+hasnaainali@users.noreply.github.com>
2024-12-09 14:55:23 +0500

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,6 @@
+public class SquareCalculator {
+    public static void main(String[] args) {
+        int number = 10; 
+        System.out.println("Square of " + number + " is " + (number * number));
+    }
+}
